eef3878d469b1fda0063d10dc87761e007847bf0
alextelerik <alextelerik@mail.bg>
2018-06-27 16:57:37 +0300

    Added some of the properties in BankOnWeb class.

--- src/thorbank/BankOnWeb.java
+++ src/thorbank/BankOnWeb.java
@@ -1,4 +1,10 @@
 package thorbank;
 
 public class BankOnWeb {
+    private int id;
+    private String email;
+    private String username;
+    private List<Bill> bill;
+    private double balance;
+
 }
