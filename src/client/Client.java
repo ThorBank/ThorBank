f0f9c5e1d027fee65205a2c2d63d2cc90546a737
siqqQ <gprex1@yahoo.com>
2018-06-28 13:28:47 +0300

    Add const values for regex(email,password, username)

--- src/client/Client.java
+++ src/client/Client.java
@@ -6,6 +6,9 @@ import java.net.PasswordAuthentication;
 import java.util.UUID;
 
 public class Client {
+    private static final String PASSWORD_REGEX = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\\\S+$).{8,}";
+    private static final String USERNAME_REGEX = "^[a-zA-Z0-9._-]{3,}$";
+    private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     private UUID id;
     private PasswordAuthentication passwordAuthentication;
     private String email;
@@ -21,17 +24,17 @@ public class Client {
     }
 
     private void setPasswordAuthentication(PasswordAuthentication passwordAuthentication) {
-        if (!passwordAuthentication.getPassword().toString().matches("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\\\S+$).{8,}")){
+        if (!passwordAuthentication.getPassword().toString().matches(PASSWORD_REGEX)){
 
         }
-        if (!passwordAuthentication.getUserName().matches("^[a-zA-Z0-9._-]{3,}$")){
+        if (!passwordAuthentication.getUserName().matches(USERNAME_REGEX)){
 
         }
         this.passwordAuthentication = passwordAuthentication;
     }
 
     private void setEmail(String email) {
-        if (!email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")){
+        if (!email.matches(EMAIL_REGEX)){
         }
         this.email = email;
     }
