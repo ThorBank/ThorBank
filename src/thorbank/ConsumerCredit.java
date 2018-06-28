5022360ffa5bcefdec5adc8a069f432e368f66a8
alextelerik <alextelerik@mail.bg>
2018-06-28 22:08:32 +0300

    Updating Credit Files

--- src/thorbank/ConsumerCredit.java
+++ src/thorbank/ConsumerCredit.java
@@ -1,14 +1,16 @@
 package thorbank;
 
 public class ConsumerCredit extends Credit {
+    private String consumer;
     private double amount;
     private int creditPeriodInMonths;
     private double yearlyInterestRate;
     private static final int MONTHS_PER_YEAR = 12;
     private static final double PERCENT_TO_NUM = 0.01d;
 
-    public ConsumerCredit(double amount, int creditPeriodInMonths, double yearlyInterestRate) {
+    public ConsumerCredit(String consumer, double amount, int creditPeriodInMonths, double yearlyInterestRate) {
         super(amount, creditPeriodInMonths);
+        this.consumer = consumer;
         this.yearlyInterestRate = yearlyInterestRate;
     }
 
