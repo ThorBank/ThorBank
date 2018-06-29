4bb0ddcb5d20cee88ee6d94729eec44a39565df6
tevetely <cvetygetova@yahoo.com>
2018-06-29 12:01:19 +0300

    Workspace

--- src/thorbank/BankOnWeb.java
+++ src/thorbank/BankOnWeb.java
@@ -12,4 +12,23 @@ public class BankOnWeb {
     private List<Transaction> transactionList;
     private List<Card> cardList;
     private List<Message> messagesWithTheBank;
+
+    public BankOnWeb(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+
+    public void changePassword(){
+
+    }
+
+    public void changeUsername(){
+
+    }
+
+}
 }
---
+++ src/thorbank/FactorConnect.java
@@ -0,0 +1,20 @@
+package thorbank;
+
+import java.util.HashMap;
+
+public class FactorConnect extends BankOnWebPro {
+
+    public FactorConnect(double balance, Bill currentBill) {
+        super(balance, currentBill);
+    }
+
+    public void plaingSalary(HashMap<Workingmen> workingmenList) {
+        for (Object workingman : workingmenList.keySet()) {
+
+        }
+    }
+
+    public void discount() {
+
+    }
+}
