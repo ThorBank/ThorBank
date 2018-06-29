b4d644cc35aadfddeec8921127899fdbb45c62a1
siqqQ <gprex1@yahoo.com>
2018-06-29 13:11:07 +0300

    Add Card classes

--- src/card/CreditCard.java
+++ src/card/CreditCard.java
@@ -1,4 +1,23 @@
 package card;
 
+import client.Client;
+import thorbank.Bill;
+
+import java.util.Date;
+
 public class CreditCard extends Card {
+    private Client client;
+
+    public CreditCard(String number, String firstName, String lastName, Date expirationDate, String CVV, String CVV2, Client client) {
+        super(number, firstName, lastName, expirationDate, CVV, CVV2);
+        setClient(client);
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 }
--- src/card/DebitCard.java
+++ src/card/DebitCard.java
@@ -1,4 +1,22 @@
 package card;
 
+import thorbank.Bill;
+
+import java.util.Date;
+
 public class DebitCard extends Card {
+    private Bill bill;
+
+    public DebitCard(String number, String firstName, String lastName, Date expirationDate, String CVV, String CVV2, Bill bill) {
+        super(number, firstName, lastName, expirationDate, CVV, CVV2);
+        setBill(bill);
+    }
+
+    public Bill getBill() {
+        return bill;
+    }
+
+    public void setBill(Bill bill) {
+        this.bill = bill;
+    }
 }
