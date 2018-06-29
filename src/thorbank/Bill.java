8d215d2c0f1a6addc83cc03d9226207d96eff5ba
alextelerik <alextelerik@mail.bg>
2018-06-29 15:59:12 +0300

    Adding Bill Class

--- src/thorbank/Bill.java
+++ src/thorbank/Bill.java
@@ -5,4 +5,43 @@ public class Bill {
     private Currency currency;
     private double balance;
     private double availability;
+
+    public Bill(String iban, Currency currency, double balance, double availability) {
+        setIban(iban);
+        setCurrency(currency);
+        setBalance(balance);
+        setAvailability(availability);
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(double availability) {
+        this.availability = availability;
+    }
 }
