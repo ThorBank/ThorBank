fc73ce718ed6a5576024f461f25dab599c1f6ef4
siqqQ <gprex1@yahoo.com>
2018-06-30 20:44:36 +0300

    Add BankTest and GringottsBankTest classes

---
+++ src/test/bank/BankTest.java
@@ -0,0 +1,10 @@
+package test.bank;
+
+
+import org.junit.Test;
+
+public class BankTest {
+    @Test
+    public void TestName() {
+    }
+}
---
+++ src/test/bank/GringottsBankTest.java
@@ -0,0 +1,17 @@
+package test.bank;
+
+import org.junit.Test;
+
+public class GringottsBankTest {
+    @Test
+    public void changeCreditInterestRateTest(){
+    }
+
+    @Test
+    public void issuanceCardTest(){
+    }
+
+    @Test
+    public void issuanceAccountTest(){
+    }
+}
