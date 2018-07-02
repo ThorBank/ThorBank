fe11a174d6ad40db23f0d7df7c49bf6b30ce9fc7
siqqQ <b.bohosyan@gmail.com>
2018-07-02 15:32:58 +0300

    Added exception to the CreditCard constructor

--- src/card/CreditCard.java
+++ src/card/CreditCard.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class CreditCard extends Card {
 
-    public CreditCard(String number, String firstName, String lastName, Date expirationDate, String CVV, String CVV2, Client client, double balance, Currency currency) {
+    public CreditCard(String number, String firstName, String lastName, Date expirationDate, String CVV, String CVV2, Client client, double balance, Currency currency) throws CardNumberFormatException {
         super(number, firstName, lastName, expirationDate, CVV, CVV2, client, balance, currency);
 
     }
